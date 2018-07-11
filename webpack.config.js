abb19cbdf62d7c9981f9c560569d4e4c0528f3fe
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2018-07-11 20:53:43 +0900

    Build

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,7 @@ module.exports = {
     extensions: ['.ts', '.tsx', '.js', '.jsx']
   },
   devtool: process.env.WEBPACK_DEVTOOL || "source-map",
+  mode: process.env.NODE_ENV,
 
   output: {
     path: distDir,
