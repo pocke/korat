9306fa9ed37f14bee4c93dd305b9f6ebf781e945
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2018-07-14 00:17:03 +0900

    Connection DB

--- src/mainProcess/index.ts
+++ src/mainProcess/index.ts
@@ -3,6 +3,7 @@ import util from 'util';
 import fs from 'fs';
 import path from 'path';
 import Electron, { ipcMain } from 'electron';
+import Datastore from 'nedb';
 
 import { Configuration } from '../share/configuration';
 import { requestConfiguration, responseConfiguration } from '../share/ipcChannels';
@@ -12,11 +13,13 @@ export default async () => {
     const conf = await loadConfiguration();
     event.sender.send(responseConfiguration, conf);
   });
+  return await initDB();
 };
 
+const HOME = process.env.HOME as string;
+
 const loadConfiguration = async () => {
-  const home = process.env.HOME as string;
-  const configPath = path.join(home, '.config/korat.yaml');
+  const configPath = path.join(HOME, '.config/korat.yaml');
 
   if (!(await fileExist(configPath))) {
     // TODO
@@ -25,6 +28,24 @@ const loadConfiguration = async () => {
 
   const content = (await util.promisify(fs.readFile)(configPath)).toString();
   return safeLoad(content) as Configuration;
+};
+
+const DBsession: {
+  events?: Nedb;
+} = {};
+
+const initDB = async () => {
+  return new Promise((resolve, reject) => {
+    const dbPath = path.join(HOME, '.cache/korat/events.nedb');
+    DBsession.events = new Datastore({ filename: dbPath });
+    DBsession.events.loadDatabase((err: Error) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
 };
 
 const fileExist = async (filePath: string) => {
