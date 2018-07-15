0a15e4abe41b84ace2cd2d93db18946425fefd71
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2018-07-15 17:23:46 +0900

    Fetch old events

--- src/mainProcess/FetchNotificationJob.ts
+++ src/mainProcess/FetchNotificationJob.ts
@@ -1,11 +1,10 @@
-import { forIn } from 'lodash';
+import { pick, forIn } from 'lodash';
 import { Headers } from 'node-fetch';
 
 import Takoneko from '../library/takoneko';
 import ConfigManager from './ConfigManager';
 import { ConfigForEndPoint } from '../share/configuration';
-import { importNotifications } from './db';
-// import { Notification } from './models/Notification';
+import { importNotifications, NotificationsSession } from './db';
 
 const sleep = (msec: number) => new Promise(resolve => setTimeout(resolve, msec));
 
@@ -27,7 +26,6 @@ export default class FetchNotificationJob {
     this.fetchLatestNotifications();
   }
 
-  // TODO: fetch old notifications
   async fetchLatestNotifications(lastModified?: string) {
     const headers = new Headers();
     if (lastModified) {
@@ -35,13 +33,33 @@ export default class FetchNotificationJob {
     }
     const { resp, body } = await this.client.notifications({ all: true, headers });
 
-    if (resp.status === 200) {
+    if (resp.status === 200 && body.length !== 0) {
+      await this.fetchOldNotifications(body[body.length - 1].updated_at);
       await importNotifications(body);
     }
     const intervalHeader = resp.headers.get('X-Poll-Interval');
-    const interval = intervalHeader ? parseInt(intervalHeader) : 60;
+    const interval = intervalHeader ? parseInt(intervalHeader) : 30;
     await sleep(interval * 1000);
     const nextLastModified = resp.headers.get('Last-Modified') || lastModified;
     this.fetchLatestNotifications(nextLastModified);
+  }
+
+  async fetchOldNotifications(lastUpdatedAt: string): Promise<undefined> {
+    console.log('fetchOldNotifications');
+
+    const { body } = await this.client.notifications({ all: true, before: lastUpdatedAt });
+
+    if (body.length !== 0) {
+      const q = pick(body[body.length - 1], ['id', 'updated_at']);
+      const exist = await NotificationsSession.conn.findOne(q, { id: 1 });
+      await importNotifications(body);
+      if (!exist) {
+        await this.fetchOldNotifications(body[body.length - 1].updated_at);
+      } else {
+        console.log('exist', exist);
+      }
+    }
+
+    return;
   }
 }
