6343f4a4d1f3384973dd1ad53f5dc4e01e2c0324
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2018-12-31 00:40:25 +0900

    Fix

--- src/renderer/ipcReceivers.ts
+++ src/renderer/ipcReceivers.ts
@@ -1,8 +1,8 @@
 import { ipcRenderer } from 'electron';
 
-import { ConfigurationChannel, IssuesChannel, UnreadCountChannel } from '../share/ipcChannels';
-import { Configuration } from '../share/configuration';
+import { IssuesChannel, UnreadCountChannel } from '../share/ipcChannels';
 import { Item } from '../share/types/SearchIssuesResult';
+import { updateIssues, updateUnreadCount } from './Actions';
 
 const def = (ch: { Response: string }, f: Function) => {
   ipcRenderer.on(ch.Response, (_event: any, ...args: any[]) => {
