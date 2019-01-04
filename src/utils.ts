48b6214bac51e4c75d385d4273c8344e9ac3cc4b
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2019-01-04 20:31:15 +0900

    Issue is issue, pull request is pull request

--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,9 @@
 import { Issue } from './renderer/API';
-
 export const issueURL = (issue: Issue, urlBase: string) => {
   const { Number, RepoOwner, RepoName } = issue;
   if (issue.IsPullRequest) {
-    return `${urlBase}/${RepoOwner}/${RepoName}/issues/${Number}`;
-  } else {
     return `${urlBase}/${RepoOwner}/${RepoName}/pull/${Number}`;
+  } else {
+    return `${urlBase}/${RepoOwner}/${RepoName}/issues/${Number}`;
   }
 };
