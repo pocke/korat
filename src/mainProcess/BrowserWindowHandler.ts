38583d5ffe44bf41e76ee5ef3a4a1225815211be
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2019-01-06 03:56:14 +0900

    Init address bar implementation

--- src/mainProcess/BrowserWindowHandler.ts
+++ src/mainProcess/BrowserWindowHandler.ts
@@ -1,84 +1,24 @@
-import { BrowserWindow, BrowserView, ipcMain, app } from 'electron';
-import { times } from 'lodash-es';
+import { BrowserWindow, ipcMain, app } from 'electron';
+import { ViewPool } from './ViewPool';
 
-let mainWindow: Electron.BrowserWindow;
+let mainWindow: BrowserWindow;
 const PREFETCH_ENABLED = process.env.KORAT_ENABLE_PREFETCH;
 
-export const setMainWindow = (w: Electron.BrowserWindow) => {
+let _ensureMainWindow: () => void;
+const ensureMainWindow = new Promise(resolve => (_ensureMainWindow = resolve));
+
+export const setMainWindow = (w: BrowserWindow) => {
   mainWindow = w;
+  _ensureMainWindow();
 };
 
-class ViewPool {
-  private pool: Array<Electron.BrowserView>;
-  private wins: Array<Electron.BrowserWindow>;
-  private openedIdx: number;
-  private headIdx: number;
-
-  constructor(private length: number) {
-    this.pool = times(this.length, () => new BrowserView({ webPreferences: { nodeIntegration: false } }));
-    this.wins = times(this.length, n => {
-      const w = new BrowserWindow({ width: 100, height: 100, show: false });
-      w.setBrowserView(this.pool[n]);
-      return w;
-    });
-
-    this.openedIdx = 0;
-    this.headIdx = 0;
-  }
-
-  async prefetch(url: string): Promise<BrowserView> {
-    const v = this.findByURL(url);
-    if (v) {
-      console.log('cache hit: ', url);
-      return v;
-    }
-    const idx = this.nextIdx();
-    const view = this.pool[idx];
-    const wc = view.webContents;
-    return new Promise((resolve, _reject) => {
-      wc.once('dom-ready', () => resolve(view));
-      wc.loadURL(url);
-    });
-  }
-
-  async open(url: string) {
-    const view = await this.prefetch(url);
-    this.openedIdx = this.pool.indexOf(view);
-    return view;
-  }
-
-  setBounds(size: Size) {
-    this.pool.forEach(v => {
-      v.setBounds(size);
-    });
-  }
-
-  private findByURL(url: string) {
-    const view = this.pool.find(v => v.webContents.getURL() === url);
-    return view;
-  }
-
-  private nextIdx(): number {
-    if (this.length === 1) return this.headIdx;
-
-    const nextIdx = this.headIdx === this.length - 1 ? 0 : this.headIdx + 1;
-    this.headIdx = nextIdx;
-    if (nextIdx === this.openedIdx) {
-      return this.nextIdx();
-    } else {
-      return nextIdx;
-    }
-  }
-
-  debug() {
-    // HACK: to avoid unused field error of this.wins
-    console.log(this.wins);
-  }
-}
-
 let viewPool: ViewPool;
-app.on('ready', () => {
+app.on('ready', async () => {
   viewPool = PREFETCH_ENABLED ? new ViewPool(15) : new ViewPool(1);
+  await ensureMainWindow;
+  viewPool.on('will-navigate', (url: string) => {
+    mainWindow.webContents.send('browser-view-will-navigate', url);
+  });
 });
 
 interface Size {
