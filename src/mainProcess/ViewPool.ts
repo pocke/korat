4d5eea4bf6fd7e7b2127407814d1d47c42143d7d
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2019-01-14 15:25:41 +0900

    Purge cache after open
    Fix #162
    
    I can more improve the cache strategy in the future.

--- src/mainProcess/ViewPool.ts
+++ src/mainProcess/ViewPool.ts
@@ -12,12 +12,14 @@ interface Size {
 export class ViewPool {
   private pool: Array<Electron.BrowserView>;
   private wins: Array<Electron.BrowserWindow>;
+  private purged: Array<boolean>;
   private openedIdx: number;
   private headIdx: number;
   private ev: EventEmitter;
 
   constructor(private length: number) {
     this.ev = new EventEmitter();
+    this.purged = times(this.length, () => false);
     this.pool = times(this.length, idx => {
       const v = new BrowserView({
         webPreferences: {
@@ -51,6 +53,7 @@ export class ViewPool {
     const idx = this.nextIdx();
     const view = this.pool[idx];
     const wc = view.webContents;
+    this.purged[idx] = false;
     return new Promise((resolve, _reject) => {
       wc.once('dom-ready', () => resolve(view));
       wc.loadURL(url);
@@ -60,6 +63,7 @@ export class ViewPool {
   async open(url: string) {
     const view = await this.prefetch(url);
     this.openedIdx = this.pool.indexOf(view);
+    this.purged[this.openedIdx] = true;
     this.ev.emit('will-navigate', url);
     return view;
   }
@@ -75,7 +79,7 @@ export class ViewPool {
   }
 
   private findByURL(url: string) {
-    const view = this.pool.find(v => v.webContents.getURL() === url);
+    const view = this.pool.find((v, idx) => !this.purged[idx] && v.webContents.getURL() === url);
     return view;
   }
 
