7c79c39085dc78f41df6c2e117017ae4da97cab0
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2019-01-18 23:34:31 +0900

    Display context menu for `a` element children

--- src/preload/preload.ts
+++ src/preload/preload.ts
@@ -3,9 +3,13 @@
 import { ipcRenderer } from 'electron';
 
 window.addEventListener('contextmenu', (ev: MouseEvent) => {
-  const target = ev.target;
-  if (target instanceof HTMLAnchorElement) {
-    ev.preventDefault();
-    ipcRenderer.send('browser-contextmenu-anchor', target.href);
+  let el = ev.target;
+  while (el) {
+    if (el instanceof HTMLAnchorElement) {
+      ev.preventDefault();
+      ipcRenderer.send('browser-contextmenu-anchor', el.href);
+    }
+
+    el = (el as Node).parentNode;
   }
 });
